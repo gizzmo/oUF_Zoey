e339217122ff27ab0bbff036338390b05a7f5cd5
Gizzmo <justgiz@gmail.com>
2015-10-05 22:00:49 -0700

    Is the menu needed anymore

--- core.lua
+++ core.lua
@@ -347,43 +347,6 @@ end
 
 
 --// Other Functions
-local dropdown = CreateFrame("Frame", "ZoeyUnitDropDownMenu", UIParent, "UIDropDownMenuTemplate")
-
-UIDropDownMenu_Initialize(dropdown, function(self)
-    local unit = self:GetParent().unit
-    if not unit then return end
-
-    local menu, name, id
-    if UnitIsUnit(unit, "player") then
-        menu = "SELF"
-    elseif UnitIsUnit(unit, "vehicle") then
-        menu = "VEHICLE"
-    elseif UnitIsUnit(unit, "pet") then
-        menu = "PET"
-    elseif UnitIsPlayer(unit) then
-        id = UnitInRaid(unit)
-        if id then
-            menu = "RAID_PLAYER"
-            name = GetRaidRosterInfo(id)
-        elseif UnitInParty(unit) then
-            menu = "PARTY"
-        else
-            menu = "PLAYER"
-        end
-    else
-        menu = "TARGET"
-        name = RAID_TARGET_ICON
-    end
-    if menu then
-        UnitPopup_ShowMenu(self, menu, unit, name, id)
-    end
-end, "MENU")
-
-local Menu = function(self)
-    dropdown:SetParent(self)
-    ToggleDropDownMenu(1, nil, dropdown, "cursor", 0, 0)
-end
-
 ns.Mouse_Focus = nil
 local function OnEnter(self)
     ns.Mouse_Focus = self
@@ -437,7 +400,6 @@ end
 oUF:RegisterStyle('Zoey', function(self, unit)
 
     --// Rightclick Menu
-    self.menu = Menu
     self:RegisterForClicks('AnyUp')
 
     --// Hover Effects
