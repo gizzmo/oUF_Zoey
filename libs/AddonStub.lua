83118bb12428b2e5d6fa65ca1cb463102f3dafa2
Gizzmo <justgiz@gmail.com>
2016-04-13 23:24:35 -0700

    We have highly modified it, so change the header

--- libs/AddonStub.lua
+++ libs/AddonStub.lua
@@ -1,43 +1,13 @@
---[[--------------------------------------------------------------------
-PhanxAddonStub
-Copyright (c) 2014-2015 Phanx <addons@phanx.net>
-All rights reserved.
+--[[----------------------------------------------------------------------------
+AddonStub.lua
 
-This file contains the entirety of the "PhanxAddonStub" software, to
-which the following license applies:
+This is a amalgamation of several addons
+	PhanxAddonStub  https://github.com/Phanx/PhanxAddonStub
+	Inomena         https://github.com/p3lim-wow/Inomena
+	TomTom          http://wowinterface.com/downloads/info7032-TomTom.html
+	Ace3            http://www.wowace.com/addons/ace3/
 
-1. Permission is granted for anyone to use, read, or otherwise interpret
-this software for any purpose, without any restrictions.
-
-2. Permission is granted for anyone to embed or include this software in
-another work that makes use of the interface provided by this software
-for the purpose of creating a package of the work and its required
-libraries, and to distribute such packages as long as the software is
-not modified in any way.
-
-3. Permission is granted for anyone to modify this software or sample
-from it, and to distribute such modified versions or derivative works
-as long as neither the names of this software nor its authors are used
-in the name or title of the work or in any other way that may cause it
-to be confused with this software or interfere with the simultaneous
-use of this software.
-
-4. This software may not be distributed standalone or in any other way,
-in whole or in part, modified or unmodified, without specific prior
-written permission from its authors.
-
-5. The names of this software and/or its authors may not be used to
-promote or endorse works derived from this software without specific
-prior written permission from the authors of this software.
-
-6. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-----------------------------------------------------------------------]]
+------------------------------------------------------------------------------]]
 
 local addonName, addon = ...
 local frame = CreateFrame("Frame")
