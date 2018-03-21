aa20da2aae1e061d01649f97be51f2f2f5b95d6e
Gizzmo <justgiz@gmail.com>
2018-03-20 20:38:07 -0700

    Update WorldMap Module
    FixWorldMapZoom doesnt always work nicely.
    Add db options

--- modules/misc/worldmap.lua
+++ modules/misc/worldmap.lua
@@ -8,7 +8,9 @@ local L = Addon.L
 -------------------------------------------------------------------- Database --
 local defaultDB = {
     profile = {
-
+        smallerWorldMap = true,
+        alphaWhileMoving = 0.35,
+        fadeWhileMoving = true,
     }
 }
 
@@ -30,16 +32,25 @@ function Module:OnInitialize()
 end
 
 function Module:OnEnable()
-    self:SecureHook("WorldMap_ToggleSizeUp", "SetLargeWorldMap")
-    self:RawHookScript(WorldMapScrollFrame, 'OnMouseWheel', 'FixWorldMapZoom')
-    BlackoutWorld:SetTexture(nil)
+    if self.db.profile.smallerWorldMap then
+        self:SecureHook("WorldMap_ToggleSizeUp", "SetLargeWorldMap")
+        self:RawHookScript(WorldMapScrollFrame, 'OnMouseWheel', 'FixWorldMapZoom')
 
-    if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
-        self:SetLargeWorldMap()
+        if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
+            self:SetLargeWorldMap()
+        end
     end
+
+    -- Set alpha used when moving
+    WORLD_MAP_MIN_ALPHA = self.db.profile.alphaWhileMoving
+    SetCVar("mapAnimMinAlpha", self.db.profile.alphaWhileMoving)
+
+    -- Enable/Disable map fading when moving
+    SetCVar("mapFade", (self.db.profile.fadeMapWhenMoving == true and 1 or 0))
 end
 
 -- Fixes how the scroll zooming in the fullscreen world map
+-- Doesnt really work. Causes taint. May not be possible. Yet
 do
     local Real_WorldMapFrame_InWindowedMode = WorldMapFrame_InWindowedMode;
     local function Fake_WorldMapFrame_InWindowedMode()
@@ -54,25 +65,27 @@ do
 end
 
 function Module:SetLargeWorldMap()
-	if InCombatLockdown() then return end
+    BlackoutWorld:SetTexture(nil)
+    WorldMapFrame:SetParent(UIParent)
+    WorldMapFrame:EnableKeyboard(false)
+    WorldMapFrame:SetScale(1)
+    WorldMapFrame:EnableMouse(true)
+    WorldMapTooltip:SetFrameStrata("TOOLTIP")
+    WorldMapCompareTooltip1:SetFrameStrata("TOOLTIP")
+    WorldMapCompareTooltip2:SetFrameStrata("TOOLTIP")
 
-	WorldMapFrame:SetParent(UIParent)
-	WorldMapFrame:EnableKeyboard(false)
-	WorldMapFrame:SetScale(1)
-	WorldMapFrame:EnableMouse(true)
-	WorldMapTooltip:SetFrameStrata("TOOLTIP")
-	WorldMapCompareTooltip1:SetFrameStrata("TOOLTIP")
-	WorldMapCompareTooltip2:SetFrameStrata("TOOLTIP")
+    if WorldMapFrame:GetAttribute('UIPanelLayout-area') ~= 'center' then
+        SetUIPanelAttribute(WorldMapFrame, "area", "center");
+    end
 
-	if WorldMapFrame:GetAttribute('UIPanelLayout-area') ~= 'center' then
-		SetUIPanelAttribute(WorldMapFrame, "area", "center");
-	end
+    if WorldMapFrame:GetAttribute('UIPanelLayout-allowOtherPanels') ~= true then
+        SetUIPanelAttribute(WorldMapFrame, "allowOtherPanels", true)
+    end
 
-	if WorldMapFrame:GetAttribute('UIPanelLayout-allowOtherPanels') ~= true then
-		SetUIPanelAttribute(WorldMapFrame, "allowOtherPanels", true)
-	end
-
-	WorldMapFrame:ClearAllPoints()
-	WorldMapFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 100)
-	WorldMapFrame:SetSize(1002, 668)
+    WorldMapFrame:ClearAllPoints()
+    WorldMapFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 100)
+    WorldMapFrame:SetSize(1002, 668)
 end
+
+-- Wrap so it happens after combat
+Module.SetLargeWorldMap = Addon:AfterCombatWrapper(Module.SetLargeWorldMap)
