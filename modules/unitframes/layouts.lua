f70b247cf95c526520f9546e665f2ff31072752d
Gizzmo <justgiz@gmail.com>
2018-04-21 14:26:42 -0700

    Update Health and Power to use the new color settings.
    ALso switch to overwriting UpdateColor instead of using a PostUpdate

--- modules/unitframes/layouts.lua
+++ modules/unitframes/layouts.lua
@@ -87,21 +87,24 @@ local function HighlightUpdate(object)
 end
 
 --------------------------------------------------------------------------------
--- Power coloring: Perfer class color, fall-back to power
-local function PostUpdatePower(Power, unit)
-    local r,g,b,t
+local function PowerUpdateColor(Power, unit, cur, min, max, displayType)
+    local db = Module.db.profile.colors
 
-    if UnitIsPlayer(unit) then
+    local r, g, b, t
+
+    if db.power_class and UnitIsPlayer(unit) then
         local class = select(2, UnitClass(unit))
         t = colors.class[class]
+    elseif db.power_class and UnitReaction(unit, 'player') then
+        t = colors.reaction[UnitReaction(unit, 'player')]
+    elseif db.power_custom then
+        t = colors.power.custom
     else
         local ptype, ptoken, altR, altG, altB = UnitPowerType(unit)
 
         t = colors.power[ptoken]
-        if(not t) then
-            if(Power.GetAlternativeColor) then
-                r, g, b = Power:GetAlternativeColor(unit, ptype, ptoken, altR, altG, altB)
-            elseif(altR) then
+        if not t  then
+            if altR then
                 r, g, b = altR, altG, altB
             else
                 t = colors.power[ptype]
@@ -116,6 +119,67 @@ local function PostUpdatePower(Power, unit)
     if b then
         Power:SetStatusBarColor(r, g, b)
         Power.bg:SetVertexColor(r*0.4, g*0.4, b*0.4)
+    end
+end
+
+local function HealthUpdateColor(Health, unit, cur, max)
+    local db = Module.db.profile.colors
+
+    local parent = Health.__owner
+    local r, g, b, t
+    if not UnitPlayerControlled(unit) and UnitIsTapDenied(unit) then
+        t = colors.tapped
+    elseif Health.disconnected then
+        t = colors.disconnected
+    elseif db.health_class then
+        if UnitIsPlayer(unit) and not db.health_force_reaction then
+            local _, class = UnitClass(unit)
+            t = colors.class[class]
+        elseif UnitReaction(unit, 'player') then
+            t = colors.reaction[UnitReaction(unit, 'player')]
+        end
+    elseif db.health_by_value then
+        r, g, b = parent.ColorGradient(cur, max, unpack(colors.smooth))
+    else
+        t = colors.health
+    end
+
+    if t  then
+        r, g, b = t[1], t[2], t[3]
+    end
+
+    if(r or g or b) then
+        Health:SetStatusBarColor(r, g, b)
+
+        local bg = Health.bg
+        if(bg) then
+
+            local t
+
+            if db.use_health_backdrop_dead and UnitIsDeadOrGhost(unit) then
+                t = db.health_backdrop_dead
+                r, g, b = t[1], t[2], t[3]
+            elseif db.health_backdrop_class then
+                local t
+                if UnitIsPlayer(unit) then
+                    local _, class = UnitClass(unit)
+                    t = colors.class[class]
+                elseif UnitReaction(unit, 'player') then
+                    t = colors.reaction[UnitReaction(unit, 'player')]
+                end
+
+                if t then
+                    r, g, b = t[1], t[2], t[3]
+                end
+            elseif db.health_backdrop_custom then
+                t = db.health_backdrop
+                r, g, b = t[1], t[2], t[3]
+            else -- defaults to a multiplier
+                r, g, b = r * 0.4, g * 0.4, b * 0.4
+            end
+
+            bg:SetVertexColor(r, g, b)
+        end
     end
 end
 
@@ -487,9 +551,7 @@ function Module:ConstructStyle(object, unit, isSingle)
     object.Health:SetPoint('RIGHT', -1, 0)
     object.Health:SetPoint('BOTTOM', 0, 1)
     object.Health.frequentUpdates = true
-    object.Health.colorTapping = true
-    object.Health.colorDisconnected = true
-    object.Health.colorHealth = true
+    object.Health.UpdateColor = HealthUpdateColor
 
     -- DispelHighlight
     object.DispelHighlight = object.Overlay:CreateTexture(nil, 'OVERLAY')
@@ -537,7 +599,7 @@ function Module:Construct_Zoey(object, unit, isSingle)
     object.Power:SetPoint('RIGHT', -1, 0)
     object.Power:SetPoint('BOTTOM', 0, 1)
     object.Power.frequentUpdates = true
-    object.Power.PostUpdate = PostUpdatePower
+    object.Power.UpdateColor = PowerUpdateColor
 
     object.Health:SetPoint('BOTTOM', object.Power, 'TOP', 0, 1)
 
@@ -1000,7 +1062,7 @@ function Module:Construct_ZoeySquare(object, unit, isSingle)
     object.Power:SetPoint('LEFT', 1, 0)
     object.Power:SetPoint('RIGHT', -1, 0)
     object.Power:SetPoint('BOTTOM', 0, 1)
-    object.Power.PostUpdate = PostUpdatePower
+    object.Power.UpdateColor = PowerUpdateColor
 
     object.Health:SetPoint('BOTTOM', object.Power, 'TOP', 0, 1)
 
